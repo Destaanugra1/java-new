1051078b8fa336ed2972abaa6b583f110aa20cbc
Desta <destaanugrapratama@gmail.com>
2025-07-31 11:24:21 +0700

    frist commit

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,22 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 10000000;
+        long iniLong = 10000000;
+        long iniLong2 = 1000000000L;
+
+        float iniFlot = 10.10F;
+        double iniDouble = 10.10;
+        int decimalInt = 25;
+        int hexInt = 0XA123B;
+        int binInt = 0b01010101;
+
+//        kode undercores
+        long amount = 1_000_000_000;
+        int balance = 25_000_000;
+
+//        kode Konversi
+
+    }
+}
